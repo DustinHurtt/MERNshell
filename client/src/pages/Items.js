d4128fcb804cd184023b900d4228671935e7940c
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-10-24 09:15:18 -0400

    continued work on items

--- client/src/pages/Items.js
+++ client/src/pages/Items.js
@@ -1,10 +1,21 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/auth.context";
 import { get, post } from "../authService/authService";
 
 const Items = () => {
 
     const { setMessage } = useContext(AuthContext)
+
+    const [allItems, setAllItems] = useState([])
+    const [item, setItem] = useState({})
+    
+    const addItem = () => {
+        setMessage(`${item} has been added to Items.`)
+    }
+
+    const deleteItem = () => {
+        setMessage(`${item} has been removed from Items.`)
+    }
 
     useEffect(() => {
         get("/items", )
@@ -14,6 +25,11 @@ const Items = () => {
 
         <div>
             <h1>This is the Items page.</h1>
+            {allItems & 
+                allItems.map((singleItem) => {
+                    return <p>{singleItem}</p>
+            })
+            }
         </div>
 
     )
